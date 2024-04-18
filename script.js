236b28d01e4476f727090a052f1bd6ec896f7b5a
EloyLeon11 <eloyribes11@gmail.com>
2024-04-18 16:05:37 +0200

    Create class

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+const request = fetch(
+    "https://fakerapi.it/api/v1/users?_quantity=20&_gender=male"
+)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("La solicitud no pudo ser completada");
+        }
+        console.log("La conexion con la API ha sido exitosa");
+        return response.json();
+    }).then((array) => {
+        const usuarios = array.data.map(user => new Usuario(user));
+
+        console.log("Usuarios Obtenidos:", usuarios)
+    }).catch((error) => {
+        console.log("Error al obtener usuarios", error)
+    });
+
 
 // Creo la clase persona
 class Persona {
@@ -36,6 +53,7 @@ class Usuario extends Persona {
     }
 }
 
+// Creo la variable persona1 con New Persona para cojer las propiedades de la clase persona y meterles valores
 const persona1 = new Persona({
     id: 1,
     firstname: "Eloy",
@@ -44,7 +62,10 @@ const persona1 = new Persona({
 
 console.log(persona1);
 
-const usuario1 = new Usuario({    
+// Creo la variable Usuario1 con New Usuario para cojer las propiedades de la clase Usuario y meterles valores.
+const usuario1 = new Usuario({   
+    
+    // person1.id significa que estoy trayendo los valores de la misma propiedad de la variable persona1.
     id: persona1.id,
     firstname: persona1.firstname,
     lastname: persona1.lastname,
